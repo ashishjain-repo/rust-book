c07286be9960de5b8a3e965dc377605835b398da
Ashish Jain <ashishjain99@yahoo.com>
2026-02-19 22:28:21 -0700

    Added Content for Ownership

--- 4/ownership/src/main.rs
+++ 4/ownership/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // How the ownership works with the variable is, they have a scope and when the variable goes out of that scope the value is dropped. These scopes are distinguished by the curly brackets
+    // here we see that A string literal is immutable but when we create a string using String type with the from function, this is going to the heap and that is why it is mutable, but string literals are not.
+    let mut s = String::from("hello");
+    s.push_str(", world");
+    println!("{s}");
+    {
+        let s = "hello";
+    }
+    // With string literals we know the space at the compile time, because they are hard coded, but with string type we have to allocate memory because they can grow.
+    // When we do String::from we are requestting the allocation
+    // Here we can see that we allocated the memory for the String but when the variable goes out of the scope, GC automatically removes/cleans it from the heap.
+    {
+        let s = String::from("String");
+    }
+
+    // Variables and Data Interacting with Move
+    
+    
 }
